a2dcbf19263927a8438f3992b3fae6ebf5b3a38e
Jesús Gutierrez <jgutierrezyancan@outlook.com>
2022-03-27 00:00:39 -0500

    avance en toma de datos de API

--- prj_prototipo/prj_prototipo/Vista/Login.xaml.cs
+++ prj_prototipo/prj_prototipo/Vista/Login.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using prj_prototipo.Modelo;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +18,31 @@ namespace prj_prototipo.Vista
         public Login()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            User user = new User
+            {
+                Email = txtEmail.Text,
+                Password = txtPassword.Text,
+            };
+            Uri RequestUri = new Uri("https://senativi-8e553-default-rtdb.firebaseio.com/Usuarios/03.json");
+            var client = new HttpClient();
+            var json = JsonConvert.SerializeObject(user);
+            var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.GetAsync(RequestUri);
+            
+
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                await Navigation.PushAsync(new nosotros());
+                //txtEmail.Text = response.Content.ReadAsStringAsync().Result;
+            } else
+            {
+                await DisplayAlert("Mensaje", "Datos Invalidos", "OK");
+            }
         }
     }
 }
